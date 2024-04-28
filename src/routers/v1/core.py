47e010fefdc4c89bd779551c04107f9198708cb5
Natan <natandiasm@gmail.com>
2024-04-28 15:56:54 -0300

    Adicionado validação de parametros passados ao endpoint

--- src/routers/v1/core.py
+++ src/routers/v1/core.py
@@ -2,7 +2,8 @@ import re
 from flask import Blueprint, request, jsonify
 from db.database import mongo
 
-from util.serializer import serialize_resultado, get_valid_filters
+from util.serializer import serialize_resultado
+from util.filters import get_valid_filters
 
 routers = Blueprint("routers_v1", __name__)
 
@@ -11,7 +12,16 @@ routers = Blueprint("routers_v1", __name__)
 def routers_v1_atendimentos():
     collection = mongo.db['atendimentos']
 
-    filtros = get_valid_filters(request.args)
+    filtros = {}
+
+    try:
+        filtros = get_valid_filters(request.args)
+
+    except ValueError as error:
+        return jsonify({'erro': str(error)}), 400
+
+    except TypeError as error:
+        return jsonify({'erro': str(error)}), 400
 
     resultados = collection.find(filtros)
 
